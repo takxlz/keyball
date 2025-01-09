51b98bfb1cf5ca747be9a3bb5411a94000bef3e1
takxlz <tak.root.yk1012@gmail.com>
2025-01-10 00:27:51 +0900

    config.hの反映

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/via-takxlz/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/via-takxlz/config.h
@@ -35,5 +35,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define TAP_CODE_DELAY 5
 
-#define POINTING_DEVICE_AUTO_MOUSE_ENABLE
-#define AUTO_MOUSE_DEFAULT_LAYER 1
+// レイヤー数
+#define DYNAMIC_KEYMAP_LAYER_COUNT 5
+
+// AML
+// #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
+// #define AUTO_MOUSE_DEFAULT_LAYER 3
