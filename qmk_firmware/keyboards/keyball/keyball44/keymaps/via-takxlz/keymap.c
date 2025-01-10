cff8e68c25dcc20af041caf7785ed1d36c952a77
takxlz <tak.root.yk1012@gmail.com>
2025-01-10 11:51:32 +0900

    キーマップの変更

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/via-takxlz/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/via-takxlz/keymap.c
@@ -25,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // デフォルトレイヤー
   [0] = LAYOUT_universal(
     KC_ESC           , LT(4,KC_Q)  , KC_W  , KC_E  , KC_R  , KC_T  ,                    KC_Y  , KC_U  , KC_I     , KC_O    , KC_P           , KC_BSPC  ,
-    LCTL_T(KC_TAB)   , LT(3,KC_A)  , KC_S  , KC_D  , KC_F  , KC_G  ,                    KC_H  , KC_J  , KC_K     , KC_L    , LT(3,KC_SCLN)  , KC_ENT  ,
+    LCTL_T(KC_TAB)   , LT(3,KC_A)  , KC_S  , KC_D  , KC_F  , KC_G  ,                    KC_H  , KC_J  , KC_K     , KC_L    , LT(3,KC_SCLN)  , RCTL_T(KC_ENT)  ,
     KC_LSFT          , KC_Z        , KC_X  , KC_C  , KC_V  , KC_B  ,                    KC_N  , KC_M  , KC_COMM  , KC_DOT  , KC_MINS        , RSFT_T(KC_INT1)  ,
             KC_LALT  , KC_LNG2     , KC_SPC ,  MO(1),  MO(2) ,                   MO(2)  ,  LT(1,KC_LNG1)  , XXXXXXX             , XXXXXXX  , KC_LGUI
   ),
@@ -34,11 +34,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______  , KC_1     , KC_2     , KC_3     , KC_4     , KC_5 ,                       KC_6     , KC_7  , KC_8  , KC_9  , KC_0     , KC_LBRC  ,
     _______  , _______  , _______  , KC_ASTR  , KC_LPRN  , _______ ,                    KC_0     , KC_4  , KC_5  , KC_6  , KC_QUOT  , KC_EQL  ,
     _______  , _______  , _______  , KC_RBRC  , KC_NUHS  , _______ ,                    _______  , KC_1  , KC_2  , KC_3  , KC_SLSH  , RSFT_T(KC_INT3)  ,
-            _______  , _______  , _______  ,        _______  , KC_DEL  ,        _______  , _______  , XXXXXXX       , XXXXXXX  , _______
+            _______  , _______  , _______  ,        _______  , _______  ,       _______  , _______  , XXXXXXX       , XXXXXXX  , _______
   ),
   // 移動レイヤー
   [2] = LAYOUT_universal(
-    _______  , _______  , _______  , _______  , _______  , _______  ,                   KC_HOME  , KC_END   , _______  , _______  , _______  , _______  ,
+    _______  , _______  , _______  , _______  , _______  , _______  ,                   KC_HOME  , KC_END   , KC_PGUP  , KC_PGDN  , _______  , _______  ,
     _______  , _______  , _______  , _______  , _______  , _______  ,                   KC_LEFT  , KC_DOWN  , KC_UP    , KC_RGHT  , _______  , _______  ,
     _______  , _______  , _______  , _______  , _______  , _______  ,                   _______  , _______  , _______  , _______  , _______  , _______  ,
             _______  , _______  , _______  ,        _______  , _______  ,       _______  , _______  , XXXXXXX       , XXXXXXX  , _______
@@ -50,11 +50,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______  , _______  , _______  , _______  , _______  , _______  ,                   _______  , KC_BTN3  , KC_BTN4  , KC_BTN6  , _______  , _______  ,
             _______  , _______  , _______  ,        _______  , _______  ,       _______  , _______  , XXXXXXX       , XXXXXXX  , _______
   ),
-  // 設定レイヤー
+  // 設定、ファンクションレイヤー
   [4] = LAYOUT_universal(
-    _______  , _______  , _______   , _______  , KBC_RST  , _______  ,                   _______  , _______  , _______  , _______  , _______  , _______ ,
-    _______  , _______  , KBC_SAVE  , _______  , _______  , _______  ,                   _______  , _______  , _______  , _______  , _______  , _______ ,
-    _______  , _______  , _______   , _______  , _______  , _______  ,                   _______  , AML_TO  , _______  , _______  , _______  , _______ ,
+    _______  , _______  , _______   , _______  , KBC_RST  , _______  ,                  KC_F10  , KC_F7  , KC_F8  , KC_F9  , KC_PSCR  , KC_DEL ,
+    _______  , AML_TO  , KBC_SAVE  , _______  , _______  , _______  ,                   KC_F11  , KC_F4  , KC_F5  , KC_F6  , _______  , _______ ,
+    _______  , _______  , _______   , _______  , _______  , _______  ,                  KC_F12  , KC_F1  , KC_F2  , KC_F3  , _______  , _______ ,
             _______  , _______  , _______  ,        _______  , _______  ,       _______  , _______  , XXXXXXX       , XXXXXXX  , _______
   ),
 };
