4d26d195f2f025f29e3409c70973b34ec65d2ad5
takxlz <tak.root.yk1012@gmail.com>
2025-01-10 15:03:06 +0900

    キーマップの変更

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/via-takxlz/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/via-takxlz/keymap.c
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC           , LT(4,KC_Q)  , KC_W  , KC_E  , KC_R  , KC_T  ,                    KC_Y  , KC_U  , KC_I     , KC_O    , KC_P           , KC_BSPC  ,
     LCTL_T(KC_TAB)   , LT(3,KC_A)  , KC_S  , KC_D  , KC_F  , KC_G  ,                    KC_H  , KC_J  , KC_K     , KC_L    , LT(3,KC_SCLN)  , RCTL_T(KC_ENT)  ,
     KC_LSFT          , KC_Z        , KC_X  , KC_C  , KC_V  , KC_B  ,                    KC_N  , KC_M  , KC_COMM  , KC_DOT  , KC_MINS        , RSFT_T(KC_INT1)  ,
-            KC_LALT  , KC_LNG2     , KC_SPC ,  MO(1),  MO(2) ,                   MO(2)  ,  LT(1,KC_LNG1)  , XXXXXXX             , XXXXXXX  , KC_LGUI
+            KC_LALT  , KC_LGUI     , LT(1,KC_LNG2) ,  KC_SPC,  LT(2,KC_ENT) ,                   LT(2,KC_BSPC)  ,  LT(1,KC_LNG1)  , XXXXXXX             , XXXXXXX  , KC_RGUI
   ),
   // 数字、記号レイヤー（スクロールモード）
   [1] = LAYOUT_universal(
