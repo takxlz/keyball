fdfb6fe9aeef25b659519970d7222e80f09ed77e
takxlz <tak.root.yk1012@gmail.com>
2025-01-14 15:52:04 +0900

    キーマップの変更

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/via-takxlz/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/via-takxlz/keymap.c
@@ -24,11 +24,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // デフォルトレイヤー
   [0] = LAYOUT_universal(
-    KC_ESC           , LT(4,KC_Q)  , KC_W  , KC_E  , KC_R  , KC_T  ,          KC_Y  , KC_U  , KC_I     , KC_O    , KC_P           , KC_BSPC  ,
-    LCTL_T(KC_TAB)   , LT(1,KC_A)  , KC_S  , KC_D  , KC_F  , KC_G  ,          KC_H  , KC_J  , KC_K     , KC_L    , LT(1,KC_SCLN)  , RCTL_T(KC_ENT)  ,
-    KC_LSFT          , LT(3,KC_Z)  , KC_X  , KC_C  , KC_V  , KC_B  ,          KC_N  , KC_M  , KC_COMM  , KC_DOT  , LT(3,KC_MINS)  , RSFT_T(KC_INT1)  ,
-    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~          ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    KC_LGUI  , KC_LALT       , LT(2,KC_LNG2) , KC_SPC  , LT(3,KC_ENT) ,          LT(3,KC_BSPC)  , LT(2,KC_LNG1)    , XXXXXXX  , XXXXXXX     , KC_RGUI
+    KC_ESC           , LT(4,KC_Q)  , KC_W          , KC_E  , KC_R  , KC_T  ,          KC_Y  , KC_U  , KC_I     , KC_O    , KC_P           , KC_BSPC  ,
+    LCTL_T(KC_TAB)   , LT(1,KC_A)  , RSFT_T(KC_S)  , KC_D  , KC_F  , KC_G  ,          KC_H  , KC_J  , KC_K     , KC_L    , LT(1,KC_SCLN)  , RCTL_T(KC_ENT)  ,
+    KC_LSFT          , LT(3,KC_Z)  , KC_X          , KC_C  , KC_V  , KC_B  ,          KC_N  , KC_M  , KC_COMM  , KC_DOT  , LT(3,KC_MINS)  , RSFT_T(KC_INT1)  ,
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~              ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    KC_LGUI  , KC_LALT       , LT(2,KC_LNG2) , LT(3,KC_ENT)  , KC_SPC ,          LT(3,KC_BSPC)  , LT(2,KC_LNG1)    , XXXXXXX  , XXXXXXX     , KC_RGUI
   ),
 
   // マウスレイヤー
