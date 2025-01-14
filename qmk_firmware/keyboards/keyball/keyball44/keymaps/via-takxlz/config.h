ad384a3f438f032877cd1bd1227cdec41932bf10
takxlz <tak.root.yk1012@gmail.com>
2025-01-15 01:08:08 +0900

    PERMISSIVE_HOLDの追加

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/via-takxlz/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/via-takxlz/config.h
@@ -35,6 +35,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define TAP_CODE_DELAY 5
 
+// Mod-Tapでホールド期間に入る前でも、他のキーが重ねられた時点でホールドとする
+#define PERMISSIVE_HOLD
+
 // レイヤー数
 #define DYNAMIC_KEYMAP_LAYER_COUNT 5
 
