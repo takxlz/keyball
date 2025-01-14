eb6b1bc097ad943f133228c57f39d4f836cf68f5
takxlz <tak.root.yk1012@gmail.com>
2025-01-15 01:48:19 +0900

    TAPPING_TERMの調整

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/via-takxlz/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/via-takxlz/config.h
@@ -35,7 +35,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define TAP_CODE_DELAY 5
 
-#define TAPPING_TERM 150
+#define TAPPING_TERM 180
 
 // Mod-Tapでホールド期間に入る前でも、他のキーが重ねられた時点でホールドとする
 #define PERMISSIVE_HOLD
