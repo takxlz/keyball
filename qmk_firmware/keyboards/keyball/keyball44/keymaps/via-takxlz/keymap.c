2d7316879209951251dbbe87d746da2372921be1
takxlz <tak.root.yk1012@gmail.com>
2025-02-01 22:09:07 +0900

    キーマップの変更

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/via-takxlz/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/via-takxlz/keymap.c
@@ -160,7 +160,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC         , KC_Q , KC_W , KC_E , KC_R , KC_T ,               KC_Y  , KC_U , KC_I    , KC_O   , KC_P            , KC_BSPC                 ,
     LCTL_T(KC_TAB) , KC_A , KC_S , KC_D , KC_F , KC_G ,               KC_H  , KC_J , KC_K    , KC_L   , KC_MINS/* -= */ , KC_ENT                  ,
     KC_LSFT        , KC_Z , KC_X , KC_C , KC_V , KC_B ,               KC_N  , KC_M , KC_COMM , KC_DOT , KC_SLSH/* /? */ , RSFT_T(KC_INT1)/* \_ */ ,
-    KC_LALT , KC_LGUI     , TD(TD_NMSM) , KC_SPC , TD(TD_MBTN) ,      MO(_MV) , TD(TD_LANG) , XXXXXXX , XXXXXXX  , KC_LBRC/* @` */
+    KC_LALT , KC_LGUI     , TD(TD_NMSM) , KC_SPC , TD(TD_MBTN) ,      MO(_MV) , TD(TD_LANG) , XXXXXXX , XXXXXXX  , LT(_MV, KC_BTN1)
   ),
 
   // マウス
@@ -173,8 +173,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   // 数字（スクロールモード）
   [_NUM] = LAYOUT_universal(
-    _______ , _______ , _______ , _______  , _______ , _______ ,         _______           , KC_7/* ' */       , KC_8/* ( */       , KC_9/* ) */     , KC_EQL/* ^~ */  , KC_BSPC          ,
-    _______ , _______ , KC_LSFT , MO(_SYM) , MO(_FN) , _______ ,         KC_0              , KC_4/* $ */       , KC_5/* % */       , KC_6/* & */     , KC_SCLN/* ;+ */ , KC_QUOT/* :* */  ,
+    _______ , _______ , _______ , _______  , _______ , _______ ,         S(KC_2)/* " */    , KC_7/* ' */       , KC_8/* ( */       , KC_9/* ) */     , KC_EQL/* ^~ */  , KC_BSPC          ,
+    _______ , _______ , KC_LSFT , MO(_SYM) , MO(_FN) , _______ ,         S(KC_7)/* ' */    , KC_4/* $ */       , KC_5/* % */       , KC_6/* & */     , KC_SCLN/* ;+ */ , KC_QUOT/* :* */  ,
     _______ , _______ , _______ , _______  , _______ , KC_SPC  ,         KC_LBRC/* @` */   , KC_1/* ! */       , KC_2/* " */       , KC_3/* # */     , KC_SLSH/* /? */ , KC_EJCT          ,
     _______ , _______           , _______  , _______ , _______ ,         C(KC_UP) , KC_0   , XXXXXXX , XXXXXXX , _______
   ),
